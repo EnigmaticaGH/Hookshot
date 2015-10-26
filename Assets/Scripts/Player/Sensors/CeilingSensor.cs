25ff034c7e9027c5d2046e4e5280209d8db25daa
Derek Shunia <eventhorizonsoundtracks@gmail.com>
2015-10-26 15:25:41 -0400

    Fixed: Issue #31 'Player phase through walls'

---
+++ Assets/Scripts/Player/Sensors/CeilingSensor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CeilingSensor : MonoBehaviour {
+
+    public RopeControl triggers;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Environment") || other.CompareTag("Hookable"))
+        {
+            triggers.setTop(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Environment") || other.CompareTag("Hookable"))
+        {
+            triggers.setTop(false);
+        }
+    }
+}
