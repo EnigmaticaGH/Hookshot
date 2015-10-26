7d8c0cac9a473a2a665a6ec22be7c5dfcb233853
Derek Shunia <eventhorizonsoundtracks@gmail.com>
2015-10-26 17:10:55 -0400

    Fixed: Issue #36 Animation not smooth

---
+++ Assets/Scripts/Player/StopGettingStuckOnWalls.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StopGettingStuckOnWalls : MonoBehaviour
+{
+    private JumpControl jump;
+    public HookshotControl hook;
+    private BoxCollider2D box;
+    public PhysicsMaterial2D friction;
+    public PhysicsMaterial2D noFriction;
+    // Use this for initialization
+    void Start()
+    {
+        box = GetComponent<BoxCollider2D>();
+        jump = GetComponentInParent<JumpControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        box.sharedMaterial = IsGrounded() ? friction : noFriction;
+    }
+
+    private bool IsGrounded()
+    {
+        return !hook.IsHooked() && jump.isGrounded();
+    }
+}
