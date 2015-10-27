022c551910bdc6bf3332e70a4a315cb111e05ebe
Derek Shunia <eventhorizonsoundtracks@gmail.com>
2015-10-27 15:55:37 -0400

    Added a very small window (80ms) in which the player can stop movement in air by tapping movement key

--- Assets/Scripts/Player/LateralMovement.cs
+++ Assets/Scripts/Player/LateralMovement.cs
@@ -6,16 +6,24 @@ public class LateralMovement : MonoBehaviour
     public float speed;
     public float force;
     public float moveForce;
+
     public HookshotControl hookshotControl;
     public SpriteRenderer characterSprite;
+    private JumpControl jump;
     private Rigidbody2D player;
     private Vector2 contactNormal;
+
     public WallSensor wallSensorRight;
     public WallSensor wallSensorLeft;
+
+    private const float AIR_STOP_TIME = 0.08f;
+    private bool canMove;
 
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
+        canMove = true;
+        jump = GetComponent<JumpControl>();
     }
 
     void FixedUpdate()
@@ -36,12 +44,19 @@ public class LateralMovement : MonoBehaviour
         {
             Vector2 lateralForce = new Vector2(horizontal * moveForce * traverse, 0);
 
-            if (Mathf.Abs(player.velocity.x) < speed)
+            if (Mathf.Abs(player.velocity.x) < speed && canMove)
                 player.AddForce(lateralForce);
 
             if (player.velocity.x > 0 && horizontal < 0
              || player.velocity.x < 0 && horizontal > 0)
+            {
                 player.velocity = new Vector2(0, player.velocity.y);
+
+                if (!jump.isGrounded())
+                {
+                    StartCoroutine(AirStopTime());
+                }
+            }
         }
         else
         {
@@ -62,6 +77,15 @@ public class LateralMovement : MonoBehaviour
             Quaternion rot = horizontal == 1 ? Quaternion.Euler(0, 0, -5.73f) : Quaternion.Euler(0, 180, -5.73f);
             characterSprite.transform.rotation = rot;
         }
+    }
+
+    IEnumerator AirStopTime()
+    {
+        canMove = false;
+
+        yield return new WaitForSeconds(AIR_STOP_TIME);
+
+        canMove = true;
     }
 
     /*bool checkWalls(float d)
