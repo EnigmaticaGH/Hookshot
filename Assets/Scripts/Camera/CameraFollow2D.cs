bdea0315d90a5d34f8d0af1516290244208508dd
JohnathanWells <slaifir@hotmail.com>
2015-11-21 05:58:32 -0500

    I added a 'simple' parallax to the camera. It probably needs some tweaking, but it's something. I also added a button to hide the background, since it made my heart hurt when playing

--- Assets/Scripts/Camera/CameraFollow2D.cs
+++ Assets/Scripts/Camera/CameraFollow2D.cs
@@ -3,7 +3,10 @@ using System.Collections;
 
 public class CameraFollow2D : MonoBehaviour
 {
+    public int numberOfBgLayers;
     public float dampTime;
+    public Transform[] Layers;
+    public float[] speedOfLayers;
     private Vector3 velocity = Vector3.zero;
     private Transform target;
     public float zoomSpeed;
@@ -15,6 +18,7 @@ public class CameraFollow2D : MonoBehaviour
     public bool LimitCameraPos;
     public Vector2 minPos;
     public Vector2 maxPos;
+    bool displayBackground = true;
 
     void Start()
     {
@@ -69,6 +73,48 @@ public class CameraFollow2D : MonoBehaviour
         destination = new Vector3(xCoordinate, yCoordinate, transform.position.z + delta.z);
 
         transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+        Parallax(destination);
 
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 300, 100, 20), "Background"))
+        {
+            HideBackground();
+        }
+    }
+
+    void Parallax(Vector3 newPos)
+    {
+        for (int a = 0; a < numberOfBgLayers; a++)
+        {
+            Layers[a].position = Vector3.SmoothDamp(Layers[a].position, new Vector3(newPos.x - (newPos.x - minPos.x) * (speedOfLayers[a]/100), newPos.y, Layers[a].position.z), ref velocity, dampTime);
+        }
+    }
+
+    void HideBackground()
+    {
+        Material material;
+        Color color;
+        int alpha = 1;
+
+        if (displayBackground == true)
+        {
+            alpha = 0;
+            displayBackground = false;
+        }
+        else
+        {
+            alpha = 1;
+            displayBackground = true;
+        }
+
+        for (int b = 0; b < numberOfBgLayers; b++)
+        {
+            material = Layers[b].GetComponent<SpriteRenderer>().material;
+            color = material.color;
+            material.color = new Color(color.r, color.g, color.b, alpha);
+        }
     }
 }
