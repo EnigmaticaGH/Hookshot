f811b58483002e6f33cc65e333bba1fe3a87f586
Derek Shunia <eventhorizonsoundtracks@gmail.com>
2015-12-06 13:13:19 -0500

    Added score keeping

--- Assets/Scripts/UI/ScoreTracker.cs
+++ Assets/Scripts/UI/ScoreTracker.cs
@@ -5,16 +5,27 @@ using System.Collections;
 public class ScoreTracker : MonoBehaviour {
 
     private static int score;
+    private static int best = 0;
+    public Text scoreText;
+    public Text bestText;
     void Awake()
     {
         InfiniteRunGenerator.Score += UpdateScore;
+        InfiniteRunGenerator.Respawn += UpdateBest;
+        bestText.text = "Best: " + best;
     }
     void OnDestroy()
     {
         InfiniteRunGenerator.Score -= UpdateScore;
+        InfiniteRunGenerator.Respawn -= UpdateBest;
     }
     void UpdateScore(int score)
     {
-        GetComponent<Text>().text = "Distance: " + score + "\nBest: ";
+        if (score > best) best = score;
+        scoreText.text = "Distance: " + score;
+    }
+    void UpdateBest()
+    {
+        bestText.text = "Best: " + best;
     }
 }
