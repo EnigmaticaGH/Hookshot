631c3c0e15e0567bf7888b0f055c898aefd7a106
JohnathanWells <slaifir@hotmail.com>
2016-01-17 04:31:27 -0500

    Pause menu, main menu, volume controls

--- Assets/Scripts/Sounds and Music/MusicScript.cs
+++ Assets/Scripts/Sounds and Music/MusicScript.cs
@@ -6,8 +6,12 @@ public class MusicScript : MonoBehaviour {
     public AudioClip[] Music;
     public AudioSource source;
 
+    public float MasterVolume = 1.0f;
+    public float MusicVolume = 1.0f;
+
 	// Use this for initialization
 	void Start () {
+        source.volume = MusicVolume * MasterVolume;
         PlayMusic(Music[0]);
 	}
 
@@ -16,5 +20,22 @@ public class MusicScript : MonoBehaviour {
         source.clip = clip;
         source.Play();
         //AudioSource.PlayClipAtPoint(clip, transform.position);
+    }
+
+    public void refreshVolume()
+    {
+        source.volume = MusicVolume * MasterVolume;
+    }
+
+    public void OnMasterChange(float newMasterVolume)
+    {
+        MasterVolume = newMasterVolume;
+        refreshVolume();
+    }
+
+    public void OnMusicChange(float newMusicVolume)
+    {
+        MusicVolume = newMusicVolume;
+        refreshVolume();
     }
 }
