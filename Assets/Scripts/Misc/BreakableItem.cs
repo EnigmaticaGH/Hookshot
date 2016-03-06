668660431663a6047c3d44f863373e7d0d70e7b0
Kamil Jarosz <kjarosz19946@gmail.com>
2016-03-05 20:16:41 -0500

    Form breakable item.

---
+++ Assets/Scripts/Misc/BreakableItem.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakableItem : MonoBehaviour {
+    public float breakableForce;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Player"))
+        {
+            // Show Particles and Shit as well
+            if(HasEnoughMomentum(other.collider.gameObject))
+                GameObject.Destroy(this);
+        }
+    }
+
+    bool HasEnoughMomentum(GameObject player)
+    {
+        Rigidbody2D playerBody = player.GetComponent<Rigidbody2D>();
+        float xVelocity = playerBody.velocity.x;
+        return xVelocity * playerBody.mass >= breakableForce;
+    }
+}
